2d3b5ae16de0847d8a184328fe7dcf423b744870
Marcelo Finkielman <marcelofinkielman@hotmail.com>
2021-03-21 12:43:19 -0700

    loop though all the comments done

--- sprint-two/scripts/bio.js
+++ sprint-two/scripts/bio.js
@@ -45,7 +45,50 @@ inputComment.classList.add('comments__form__divComment__input')
 divComment.appendChild(inputComment)
 
 let commentButton = document.createElement('button')
-commentButton.classList.add('comments__form__commentButton')
+commentButton.classList.add('comments__form__divComment__commentButton')
 commentButton.innerText = 'COMMENT'
-form.appendChild(commentButton)
+divComment.appendChild(commentButton)
 
+let postedComments = document.createElement('div')
+postedComments.classList.add('comments__postedComments')
+comments.appendChild(postedComments)
+
+
+let defaultComments = [
+  {
+      name: 'Micheal Lyons',
+      date: '12/12/2018',
+      comment: 'They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.'
+  },
+  {
+      name: 'Gary Wong',
+      date: '12/18/2018',
+      comment: 'Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!'
+  },
+  {
+      name: 'Theodore Duncan',
+      date: '12/18/2018',
+      comment: 'How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!'
+  }
+];
+
+function displayComments() {
+  for (let i = 0; i < defaultComments.length; i++) {
+      let commentsName = document.createElement('p');
+      commentsName.classList.add('dishes__name');
+      commentsName.innerText = defaultComments[i].name;
+      postedComments.appendChild(commentsName);
+  
+      let commentsDate = document.createElement('p');
+      commentsDate.classList.add('dishes__cuisine');
+      commentsDate.innerText = defaultComments[i].date;
+      postedComments.appendChild(commentsDate);
+  
+      let commentsComment = document.createElement('p');
+      commentsComment.classList.add('dishes__commentsComment');
+      commentsComment.innerText = defaultComments[i].comment;
+      postedComments.appendChild(commentsComment);
+  }
+};
+
+displayComments();
