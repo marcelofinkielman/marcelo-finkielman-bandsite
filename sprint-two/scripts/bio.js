08e2d2c3523e0e7af7d5eb07481adc9e776971dd
Marcelo Finkielman <marcelofinkielman@hotmail.com>
2021-03-21 17:29:36 -0700

    functions for commentary done

--- sprint-two/scripts/bio.js
+++ sprint-two/scripts/bio.js
@@ -49,46 +49,63 @@ commentButton.classList.add('comments__form__divComment__commentButton')
 commentButton.innerText = 'COMMENT'
 divComment.appendChild(commentButton)
 
-let postedComments = document.createElement('div')
-postedComments.classList.add('comments__postedComments')
-comments.appendChild(postedComments)
+let allComments = document.createElement('div')
+allComments.classList.add('comments__allComments')
+comments.appendChild(allComments)
+
+
 
 
 let defaultComments = [
   {
-      name: 'Micheal Lyons',
-      date: '12/12/2018',
-      comment: 'They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.'
+    name: 'Micheal Lyons',
+    date: '12/18/2018',
+    comment: 'They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.'
   },
   {
-      name: 'Gary Wong',
-      date: '12/18/2018',
-      comment: 'Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!'
+    name: 'Gary Wong',
+    date: '12/12/2018',
+    comment: 'Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!'
   },
   {
-      name: 'Theodore Duncan',
-      date: '12/18/2018',
-      comment: 'How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!'
+    name: 'Theodore Duncan',
+    date: '11/15/2018',
+    comment: 'How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!'
   }
 ];
-
+//function for the name, date comment in the comments section.
+let commentArea = document.querySelector('.comments__allComments')
 function displayComments() {
-  for (let i = 0; i < defaultComments.length; i++) {
-      let commentsName = document.createElement('p');
-      commentsName.classList.add('dishes__name');
-      commentsName.innerText = defaultComments[i].name;
-      postedComments.appendChild(commentsName);
   
-      let commentsDate = document.createElement('p');
-      commentsDate.classList.add('dishes__cuisine');
-      commentsDate.innerText = defaultComments[i].date;
-      postedComments.appendChild(commentsDate);
-  
-      let commentsComment = document.createElement('p');
-      commentsComment.classList.add('dishes__commentsComment');
-      commentsComment.innerText = defaultComments[i].comment;
-      postedComments.appendChild(commentsComment);
-  }
+
+  for (let i = defaultComments.length -1; i >= 0; i--) {
+    addComment(defaultComments[i])
+
+  };
 };
 
 displayComments();
+
+document.querySelector("form.comments__form button").onclick = function (event) {
+  event.preventDefault()
+}
+
+function addComment(comment) {
+  let commentContainer = document.createElement('div');
+  commentContainer.classList.add('comments__allComments__commentContainer')
+  let commentsName = document.createElement('p');
+  commentsName.classList.add('comments__allComments__commentContainer__name');
+  commentsName.innerText = comment.name;
+  commentContainer.appendChild(commentsName);
+  let commentsDate = document.createElement('p');
+  commentsDate.classList.add('comments__allComments__commentContainer__date');
+  commentsDate.innerText = comment.date;
+  commentContainer.appendChild(commentsDate);
+  let commentsComment = document.createElement('p');
+  commentsComment.classList.add('comments__allComments__commentCointainer__commentary');
+  commentsComment.innerText = comment.comment;
+  commentContainer.appendChild(commentsComment);
+  commentArea.insertBefore(commentContainer, commentArea.firstChild)
+  
+  
+}
