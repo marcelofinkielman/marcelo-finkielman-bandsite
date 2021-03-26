cdd7f0a5fe34f1ce0b28e32da8355ca597d410c0
Marcelo Finkielman <marcelofinkielman@hotmail.com>
2021-03-25 23:55:01 -0700

    saving before big change

--- sprint-two/scripts/bio.js
+++ sprint-two/scripts/bio.js
@@ -3,48 +3,48 @@
 let comments = document.querySelector('.comments')
 
 let header = document.createElement('p')
-header.classList.add('commentsHeader')
+header.classList.add('comments__header')
 header.innerText = 'Join the Conversation'
 comments.appendChild(header)
 
 let form = document.createElement('form')
-form.classList.add('commentsForm')
+form.classList.add('comments__form')
 comments.appendChild(form)
 
 
 let divName = document.createElement('div')
-divName.classList.add('commentsForm__divName')
+divName.classList.add('comments__form__divName')
 form.appendChild(divName)
 
 let profile = document.createElement('img')
-profile.classList.add('commentsForm__divName-profile')
+profile.classList.add('comments__form__divName-profile')
 profile.src = '../Assets/Images/profile.jpg'
 divName.appendChild(profile)
 
 let labelName = document.createElement('label')
-labelName.classList.add('commentsForm__divName-labelName')
+labelName.classList.add('comments__form__divName-labelName')
 labelName.innerText = 'Name'
 divName.appendChild(labelName)
 
 let inputName = document.createElement('input')
-inputName.classList.add('commentsForm__divName-input')
+inputName.classList.add('comments__form__divName-input')
 divName.appendChild(inputName)
 
 let divComment = document.createElement('div')
-divComment.classList.add('commentsForm__divComment')
+divComment.classList.add('comments__form__divComment')
 form.appendChild(divComment)
 
 let labelComment = document.createElement('label')
-labelComment.classList.add('commentsForm__divComment-labelComment')
+labelComment.classList.add('comments__form__divComment-labelComment')
 labelComment.innerText = 'Comment'
 divComment.appendChild(labelComment)
 
 let inputComment = document.createElement('textarea')
-inputComment.classList.add('commentsForm__divComment-input')
+inputComment.classList.add('comments__form__divComment-input')
 divComment.appendChild(inputComment)
 
 let commentButton = document.createElement('button')
-commentButton.classList.add('commentsForm__divComment-commentButton')
+commentButton.classList.add('comments__form__divComment-commentButton')
 commentButton.innerText = 'COMMENT'
 divComment.appendChild(commentButton)
 
@@ -94,7 +94,7 @@ let addComment = (comment) => {
   commentsName.innerText = comment.name
   nameDate.appendChild(commentsName)
   let commentsDate = document.createElement('p')
-  commentsDate.classList.add('comments-allComments__commentContainer__nameDate-date')
+  commentsDate.classList.add('comments-allComments__commentContainer__containAll__nameDate-date')
   commentsDate.innerText = newDate(comment.timestamp)
   nameDate.appendChild(commentsDate)
   let commentsComment = document.createElement('p')
@@ -121,14 +121,14 @@ let newDate = (commentsTimestamp) => {
 
 
 
-let submitForm = document.querySelector('.commentsForm')
+let submitForm = document.querySelector('.comments__form')
 
 submitForm.addEventListener('submit', function (event) {
   event.preventDefault();
 
 
-  let newName = event.target.querySelector('.commentsForm__divName-input').value
-  let newComment = event.target.querySelector('.commentsForm__divComment-input').value
+  let newName = event.target.querySelector('.comments__form__divName-input').value
+  let newComment = event.target.querySelector('.comments__form__divComment-input').value
   
 
   
